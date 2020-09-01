718f8d6bf0c12be97c64b50396e78d2b224ba996
TheEZIC <35061249+TheEZIC@users.noreply.github.com>
2020-09-01 17:42:12 +0300

    Fix mania

--- src/Mania/ManiaDifficulty.ts
+++ src/Mania/ManiaDifficulty.ts
@@ -8,87 +8,122 @@ interface ManiaDifficultyHitObject {
     overallStrain: number;
 }
 
-const 
-    weightDecayBase = 0.9,
-    individualDecayBase = 0.125,
-    overallDecayBase = 0.3,
-    starScalingFactor = 0.018;
+export default function CalculateDifficulty(
+    beatmap: ManiaBeatmap,
+    timeScale: number
+): ManiaDifficulty {
+    //console.log(timeScale)
+    return new ManiaDifficultyCalculator(beatmap, 1).diff;
+}
 
-export default function CalculateDifficulty(beatmap: ManiaBeatmap, timeScale: number = 1): ManiaDifficulty {
-    const strainStep = 400 * timeScale;
-    
-    let heldUntil = new Array(beatmap.Stats.CS).fill(0);
+class ManiaDifficultyCalculator {
+    private readonly INDIVIDUAL_DECAY_BASE = 0.125;
+    private readonly OVERALL_DECAY_BASE = 0.3;
+    private readonly STAR_SCALING_FACTOR = 0.018;
 
-    let notes: ManiaDifficultyHitObject[] = beatmap.HitObjects.map(note => ({ 
-        note, 
-        individualStrain: new Array(beatmap.Stats.CS).fill(0),
-        overallStrain: 1
-    }));
+    private notes: ManiaDifficultyHitObject[];
 
-    let previousObject = notes[0];
-    
-    for(let object of notes.slice(1)) {
-        let delta = (object.note.StartTime - previousObject.note.StartTime) / timeScale / 1e3;
-        let individualDecay = Math.pow(individualDecayBase, delta);
-        let overallDecay = Math.pow(overallDecayBase, delta);
-        
-        let holdFactor = 1,
-            holdAddition = 0;
+    public readonly diff: ManiaDifficulty;
 
-        for(let i = 0; i < beatmap.Stats.CS; i++) {
-            heldUntil[i] = previousObject.note.EndTime;
+    constructor(
+        private readonly beatmap: ManiaBeatmap,
+        private readonly timeScale: number = 1
+    ) {
+        this.initObjects();
+        this.computeStrains();
+        const highestStains: number[] = this.calcHighestStrains();
+        const difficulty: number = this.calcDifficulty(highestStains);
 
-            if(object.note.StartTime < heldUntil[i] && object.note.EndTime > heldUntil[i])
-                holdAddition = 1;
-            else if(object.note.EndTime == heldUntil[i])
-                holdAddition = 0;
-            else if(object.note.EndTime < heldUntil[i])
-                holdFactor = 1.25;
-
-            object.individualStrain[i] = previousObject.individualStrain[i] * individualDecay;
-        }
-        
-        heldUntil[object.note.Column - 1] = object.note.EndTime;
-
-        object.individualStrain[object.note.Column - 1] += 2 * holdFactor;
-        object.overallStrain = previousObject.overallStrain * overallDecay + (1 + holdAddition) * holdFactor;
-
-        previousObject = object;
+        this.diff = new ManiaDifficulty(difficulty * this.STAR_SCALING_FACTOR);
     }
 
-    let strainTable = [],
-        maxStrain = 0,
-        intervalEndTime = strainStep;
+    private initObjects() {
+        this.notes = this.beatmap.HitObjects.map(note => ({ 
+            note, 
+            individualStrain: new Array(this.beatmap.Stats.CS).fill(0),
+            overallStrain: 1
+        }));
 
-    previousObject = null;
+        this.notes.sort((a, b) => a.note.StartTime - b.note.StartTime);
+    }
 
-    for(let object of notes) {
-        while(object.note.StartTime > intervalEndTime) {
-            strainTable.push(maxStrain);
-            if(!previousObject) maxStrain = 0;
-            else {
-                let individualDecay = Math.pow(individualDecayBase, (intervalEndTime - previousObject.note.StartTime) / 1e3);
-                let overallDecay = Math.pow(overallDecayBase, (intervalEndTime - previousObject.note.StartTime) / 1e3);
-                maxStrain = previousObject.individualStrain[previousObject.note.Column - 1] * individualDecay + previousObject.overallStrain * overallDecay;
+    private computeStrains() {
+        let prev: ManiaDifficultyHitObject = this.notes[0];
+        let heldUntil: number[] = new Array(this.beatmap.Stats.CS).fill(0);
+
+        for(let object of this.notes.slice(1)) {
+            let delta: number = (object.note.StartTime - prev.note.StartTime) / this.timeScale / 1e3;
+            let individualDecay: number = this.INDIVIDUAL_DECAY_BASE ** delta;
+            let overallDecay: number = this.OVERALL_DECAY_BASE ** delta;
+            
+            let holdFactor: number = 1,
+                holdAddition: number = 0;
+            
+            for(let i = 0; i < this.beatmap.Stats.CS; i++) {
+                if(object.note.StartTime < heldUntil[i] && object.note.EndTime > heldUntil[i])
+                    holdAddition = 1;
+                else if(object.note.EndTime == heldUntil[i])
+                    holdAddition = 0;
+                else if(object.note.EndTime < heldUntil[i])
+                    holdFactor = 1.25;
+
+                object.individualStrain[i] = prev.individualStrain[i] * individualDecay;
             }
-            intervalEndTime += strainStep;
+            
+            let column = object.note.Column - 1;
+
+            heldUntil[column] = object.note.EndTime;
+
+            object.individualStrain[column] += 2 * holdFactor;
+            object.overallStrain = prev.overallStrain * overallDecay + (1 + holdAddition) * holdFactor;
+            prev = object;
         }
-        let strain = object.individualStrain[object.note.Column - 1] + object.overallStrain;
-        maxStrain = Math.max(maxStrain, strain);
-        previousObject = object;
     }
 
-    let difficulty = 0,
-        weight = 1;
+    private calcHighestStrains(): number[] {
+        let sectionLen: number = 400 * this.timeScale;
+        let curSectionEnd: number = Math.ceil(this.notes[0].note.StartTime / sectionLen) * sectionLen;
+        let curSectionPick = 0;
+        let highestStains: number[] = [];
 
-    strainTable.sort((a, b) => b - a);
+        let prev: ManiaDifficultyHitObject = null;
+
+        for(let o of this.notes) {
+            while(o.note.StartTime > curSectionEnd) {
+                highestStains.push(curSectionPick);
+
+                if(!prev) curSectionPick = 0;
+                else {
+                    let individualDecay = Math.pow(this.INDIVIDUAL_DECAY_BASE, (curSectionEnd - prev.note.StartTime) / 1e3);
+                    let overallDecay = Math.pow(this.OVERALL_DECAY_BASE, (curSectionEnd - prev.note.StartTime) / 1e3);
+                    curSectionPick = prev.individualStrain[prev.note.Column - 1] * individualDecay + prev.overallStrain * overallDecay;
+                }
+
+                curSectionEnd += sectionLen;
+            }
+
+            let strain = o.individualStrain[o.note.Column - 1] + o.overallStrain;
+            curSectionPick = Math.max(curSectionPick, strain);
+
+            prev = o;
+        }
+
+        return highestStains.sort((a, b) => b - a);
+    }
+
+    private calcDifficulty(
+        highestStains: number[]
+    ): number {
+        let difficulty: number = 0,
+            weight: number = 1;
     
-    for(let strain of strainTable) {
-        difficulty += strain * weight;
-        weight *= weightDecayBase;
-    }
+        for(let strain of highestStains) {
+            difficulty += strain * weight;
+            weight *= 0.9;
+        }
 
-    return new ManiaDifficulty(difficulty * starScalingFactor);
+        return difficulty;
+    }
 }
 
 export class ManiaDifficulty extends Difficulty {
